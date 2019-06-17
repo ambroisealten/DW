9b449190f5020616b00716954e6c1af4842f375e
lucasroyackkers <48323570+lucasroyackkers@users.noreply.github.com>
2019-06-17 11:54:54 +0200

    Chart container can be divided in 4 separate containers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DW - Lot 0';
+  containerRepeat : number = 1;
+
 
   diviseChartsSegment(){
     console.log("On veut diviser ici");
-    let chartContainer = document.getElementById('chartContainer');
+
+    
+    let chartContainer = document.getElementById('chartContainerSimple') == null ? document.getElementById('chartContainerDouble') : document.getElementById('chartContainerSimple');
+
+    this.containerRepeat += 1;
+
+    if(this.containerRepeat > 4){
+      this.containerRepeat = 4;
+    }
+    else if(this.containerRepeat > 2){
+      if(this.containerRepeat == 3){
+        this.resizeAllCharts();
+      }
+      let newDivForChart = document.createElement('div');
+      newDivForChart.setAttribute('class',"chartsFour");
+
+      chartContainer.setAttribute('id','chartContainerDouble');
+      chartContainer.appendChild(newDivForChart);
+    }
+    else{
+      let newDivForChart = document.createElement('div');
+      newDivForChart.setAttribute('class',"charts");
+  
+      chartContainer.setAttribute('id','chartContainerDouble');
+      chartContainer.appendChild(newDivForChart);
+    }
+
+
+    
+  }
+
+  resizeAllCharts(){
+    let allCharts = document.getElementsByClassName('charts');
+
+    let arr = Array.from(allCharts);
+
+    arr.forEach( chart => {
+      chart.setAttribute("class","chartsFour");
+    })
   }
 }

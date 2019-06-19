0983ebc401da8b82f4f951544d4a3447dda95af7
Andy CHABALIER <andy.chabalier@alten.com>
2019-06-19 11:27:53 +0200

    implementation of data scheme accordion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { DataService } from './services/dataService';
 import { ViewService } from './services/viewService';
 import { ChartViewComponent } from './components/chart-view/chart-view.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatExpansionModule } from '@angular/material/expansion';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
@@ -15,6 +18,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     HttpClientModule,
+    MatExpansionModule,
+    BrowserAnimationsModule
   ],
   providers: [
     DataService,

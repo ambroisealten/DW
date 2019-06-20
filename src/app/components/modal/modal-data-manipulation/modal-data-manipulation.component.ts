09b1fd42591b0e3b7c1fc289d535da3c500454a2
Quentin_DP_ALTEN <quentin.dellapasqua@alten.com>
2019-06-20 16:48:08 +0200

    Ajout logique bouton

--- src/app/components/modal/modal-data-manipulation/modal-data-manipulation.component.ts
+++ src/app/components/modal/modal-data-manipulation/modal-data-manipulation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { MatTableDataSource, MatSelectionList, MatSelectionListChange, MatCheckbox } from '@angular/material';
 import { SelectionModel } from '@angular/cdk/collections';
@@ -32,11 +32,11 @@ export class ModalDataManipulationComponent implements OnInit {
 
   isString: boolean;
 
-  @ViewChild('data', { static: true }) type: MatSelectionList;
+  @ViewChild('type', { static: true }) type: MatSelectionList;
   @ViewChild('compris',{ static: true}) compris: MatCheckbox ;
+  
   constructor(private dialogRef: MatDialogRef<ModalDataManipulationComponent>, @Inject(MAT_DIALOG_DATA) public data) {
     this.isString = data.bool;
-    console.log(this.isString);
   }
 
   ngOnInit() {
@@ -47,26 +47,8 @@ export class ModalDataManipulationComponent implements OnInit {
     });
   }
 
-  /** Whether the number of selected elements matches the total number of rows. */
-  isAllSelected() {
-    const numSelected = this.selection.selected.length;
-    const numRows = this.dataSource.data.length;
-    return numSelected === numRows;
-  }
-
-  /** Selects all rows if they are not all selected; otherwise clear selection. */
-  masterToggle() {
-    this.isAllSelected() ?
-      this.selection.clear() :
-      this.dataSource.data.forEach(row => this.selection.select(row));
-  }
-
-  /** The label for the checkbox on the passed row */
-  checkboxLabel(row?: PeriodicElement): string {
-    if (!row) {
-      return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
-    }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.position + 1}`;
+  unselectOther(){
+    this.type.deselectAll() ;
   }
 
   close() {

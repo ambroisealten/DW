29aa7ec3908ed0c9581d37c210ea013fb4302196
Andy CHABALIER <andy.chabalier@alten.com>
2019-06-24 10:37:02 +0200

    Hot fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,10 @@ import { ChartViewComponent } from './components/chart-view/chart-view.component
 import { DataService } from './services/dataService';
 import { ViewService } from './services/viewService';
 
-import { ChartViewComponent } from './components/chart-view/chart-view.component';
 import { ModalDataManipulationComponent } from './components/modal/modal-data-manipulation/modal-data-manipulation.component';
 import { ParamViewComponent } from './components/param-view/param-view.component';
 import { MatTableModule, MatRadioModule, MatFormFieldModule, MatSlideToggleModule, MatCheckboxModule, MatTabsModule, MatDialogModule, MatSelectModule, MatSliderModule, MatListModule, MatInputModule } from '@angular/material';
 import { MatIconModule } from '@angular/material/icon';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ModalStringManipulationComponent } from './components/modal/modal-string-manipulation/modal-string-manipulation.component';
 import { FormsModule } from '@angular/forms';
 
@@ -41,7 +39,7 @@ import { FormsModule } from '@angular/forms';
     MatIconModule,
     MatRadioModule,
     MatInputModule,
-    FormsModule
+    FormsModule,
     HttpClientModule,
     MatExpansionModule,
     BrowserModule,

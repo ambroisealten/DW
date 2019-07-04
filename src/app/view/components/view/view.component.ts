ef391008041924ad30df036d28d1edcff54cb9c3
Quentin_DP_ALTEN <quentin.dellapasqua@alten.com>
2019-07-04 16:17:27 +0200

    Correction routing Man's not hot

--- src/app/view/components/view/view.component.ts
+++ src/app/view/components/view/view.component.ts
@@ -8,20 +8,67 @@ import { LoadEcranService } from '../../services/load-ecran.service';
 })
 export class ViewComponent implements OnInit {
 
-  allTemplates = 0 ; 
+  allTemplates = 0;
+  containerRepeat = 1;
 
   //Liste des entries pour les templates
   @ViewChildren('chartHost', { read: ViewContainerRef }) entries: QueryList<ViewContainerRef>;
 
   constructor(private loadEcranService: LoadEcranService,
     private componentFactoryResolver: ComponentFactoryResolver,
-    ) { }
+  ) { }
 
   ngOnInit() {
-    this.loadEcranService.loadEcran().subscribe((data: any[]) => 
-      {
-        this.allTemplates = data.length ; 
-      }) ; 
+    this.loadEcranService.loadEcran().subscribe((data: any[]) => {
+      this.allTemplates = data.length;
+    });
+  }
+
+  /**
+ * Parcourt le div contenant les templates disponibles, et retourne la première contenue dans ce div
+ * @param idNumber 
+ */
+  parseTemplateDiv() {
+    let container = document.getElementById('templates');
+    let test = container.firstChild;
+    while (test.nodeName != "TEMPLATE") {
+      test = test.nextSibling;
+    }
+    return test;
+  }
+
+  /**
+   * Permet d'ajouter les charts au DOM et de le dimensionner différemment selon le nombre de chart
+   */
+  diviseChartsSegment() {
+    const chartContainer = document.getElementById('chartContainerSimple') == null ?
+      document.getElementById('chartContainerDouble') : document.getElementById('chartContainerSimple');
+
+    const allChartChilds = document.getElementsByTagName('nav')[0].nextSibling.childNodes.length;
+
+    this.containerRepeat += 1;
+    const newDivForChart = document.createElement('div');
+    newDivForChart.setAttribute('id', this.containerRepeat.toString());
+    const template = this.parseTemplateDiv();
+    newDivForChart.appendChild(template);
+    if (allChartChilds < 2) chartContainer.setAttribute('id', 'chartContainerSimple');
+    else chartContainer.setAttribute('id', 'chartContainerDouble');
+
+    if (allChartChilds > 2) {
+      newDivForChart.setAttribute('class', 'chartsFour');
+      chartContainer.appendChild(newDivForChart);
+
+    }
+    else {
+      newDivForChart.setAttribute('class', 'charts');
+
+      chartContainer.appendChild(newDivForChart);
+    }
+    let latestActive = document.getElementsByClassName('containerActive')[0];
+    if (latestActive != null) {
+      let latestActiveClass = latestActive.getAttribute('class').split('containerActive')[1];
+      latestActive.setAttribute('class', latestActiveClass);
+    }
   }
 
 }

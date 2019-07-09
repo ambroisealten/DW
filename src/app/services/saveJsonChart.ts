43d72355f60ef42cf285a223b383bf9b9bac4f3c
lucasroyackkers <48323570+lucasroyackkers@users.noreply.github.com>
2019-07-09 09:40:45 +0200

    Update saveJsonChart.ts

--- src/app/services/saveJsonChart.ts
+++ src/app/services/saveJsonChart.ts
@@ -2,18 +2,33 @@ import { environment } from 'src/environments/environment';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { SaveChart } from '../models/saveCharts';
 
 @Injectable()
 export class SaveJsonCharts {
 
     constructor(private httpClient: HttpClient) { }
 
-    fetchDataScheme(): Observable<any> {
+    saveChartConfig(saveChart: SaveChart): Observable<any> {
         const headers = new HttpHeaders({
             'Content-Type': 'application/json',
         })
-        return this.httpClient.get()
+
+        return this.httpClient
+        .post<SaveChart>(environment.baseUrl+ '/saveChartConfig/displayedName', saveChart,  {headers})
     }
 
-
+    getChartConfig(chartId: number, displayedName: string) {
+        const headers = new HttpHeaders({
+            'Content-Type': 'application/json',
+        });
+        return this.httpClient.get(
+            environment.baseUrl
+            + '/getChartConfig/'
+            + chartId
+            + '/'
+            + displayedName
+            + '/'
+            , { headers});
+    }
 }
